a5582883592cee28677f9513c77dadfe8af2ad17
Abel <Abel@192.168.1.139>
2022-01-17 20:12:20 +0100

    Main preparado

--- src/main/java/es/ideas/App.java
+++ src/main/java/es/ideas/App.java
@@ -2,33 +2,23 @@ package es.ideas;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-/**
- * JavaFX App
- */
 public class App extends Application {
-
-    private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
+        Scene escena;
+
+        escena = new Scene(FXMLLoader.load(App.class.getResource("view/PaletaColoresFXML.fxml")));
+
+        stage.setScene(escena);
+        stage.setTitle("Paleta de colores");
+        stage.setResizable(false);
         stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
